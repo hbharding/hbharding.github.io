a8786da40348e5aac79f6a0203f90881c265c149
hbharding <hbharding@gmail.com>
2014-02-04 01:30:54 -0500

    helix code

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  // Initialize config.
+  grunt.initConfig({
+    pkg: require('./package.json'),
+    meta: {
+      port: '3000',
+      serverPath: 'index.js',
+      dirs: {
+        // root: '.',
+        // public: './assets',
+        // css: './assets/custom/css',
+        // images: './assets/custom/images',
+        // js: './assets/custom/js',
+        // sass: './sass',
+      }
+    }
+  });
+
+  // Load per-task config from separate files.
+  grunt.loadTasks('./grunt');
+
+  // Register alias tasks.
+  // grunt.registerTask('build',
+  //   'Build site files for testing or deployment.',
+  //   ['jshint', 'clean', 'jade:prod', 'requirejs:prod', 'stylus:prod']);
+
+  // grunt.registerTask('deploy',
+  //   'Deploy site via gh-pages.',
+  //   ['build', 'gh-pages']);
+
+  grunt.registerTask('dev',
+    'Start an express webserver on localhost.',
+    ['express']);
+
+  // grunt.registerTask('prod',
+  //   'Publish to build/wwwroot and start a webserver on localhost.',
+  //   ['build', 'connect:prod:keepalive']);
+
+  grunt.registerTask('default', ['dev']);
+
+};
